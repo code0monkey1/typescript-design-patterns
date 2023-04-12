569a1940a1c52cd7d31b6d5804879b5bfe187e8a
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-12 07:04:23 +0000

    12 Apr 2023, 12:34

--- src/Creational/Factory/InMemoryDatabase.ts
+++ src/Creational/Factory/InMemoryDatabase.ts
@@ -1,11 +1,24 @@
-
-
-interface BaseType{
+type BaseType={
   id:string
 }
+type Pokemon={
+  id:string,
+  attack:number,
+  power:number
+}
 
-interface Pokemon{
-  id:"Bulbasaur",
-  attack:10,
-  power:20
+type Database <T extends BaseType> ={
+     set(newValue:T):void
+     get(id:string):T|undefined
+}
+
+
+class InMemoryDatabase<T extends BaseType> implements Database<T> {
+  
+   set(newValue: T): void {
+       
+   }
+   get(id: string): T | undefined {
+       return undefined
+   }
 }
