c53a5145dbf5efca6b1581c8fa2aa3473ffba7f9
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-12 07:10:04 +0000

    12 Apr 2023, 12:40

--- src/Creational/Factory/InMemoryDatabase.ts
+++ src/Creational/Factory/InMemoryDatabase.ts
@@ -12,13 +12,26 @@ type Database <T extends BaseType> ={
      get(id:string):T|undefined
 }
 
-
 class InMemoryDatabase<T extends BaseType> implements Database<T> {
-  
+    
+   private db :Record<string,T> ={};
+   
    set(newValue: T): void {
-       
+       this.db[newValue.id] = newValue;
    }
    get(id: string): T | undefined {
-       return undefined
+       return this.db[id];
    }
 }
+
+
+const pokemonDb = new InMemoryDatabase<Pokemon>();
+
+pokemonDb.set({
+  id:"bulbasaur",
+  attack:12,
+  power:14
+})
+
+console.log(pokemonDb.get("bulbasaur"))
+
