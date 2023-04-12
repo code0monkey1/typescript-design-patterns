5ec9921939a41b948e58384d0f5aec56de9ba447
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-12 08:02:22 +0000

    12 Apr 2023, 13:32

--- src/Creational/Factory/InMemoryDatabase.ts
+++ src/Creational/Factory/InMemoryDatabase.ts
@@ -1,13 +1,6 @@
-interface DatabaseServices<T,R>{
-
-   create(data:T):R|undefined
-   read(id:string):R|undefined
-   update(data:T):R|undefined
-   delete(id:string):R|undefined
+abstract class Database{
 
 }
-
-
 
 
 
