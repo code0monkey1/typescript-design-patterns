62a778a70cc9f59e693bcf3c305b07a05e74ba39
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-12 06:15:10 +0000

    12 Apr 2023, 11:45

--- src/Creational/Factory.ts
+++ src/Creational/Factory.ts
@@ -20,7 +20,7 @@ interface CarFactory < T extends Car>{
    
 }
 
-function getCar<T>(){
+ getCar<T extends Car> =()=>{
   class TeslaCarFactory<T extends Car> implements CarFactory<T >{
       
       introduceYourself(car: T): void {
