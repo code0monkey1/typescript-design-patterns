d928215abf5535658f3d441d9b80225c71031dbd
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-12 07:43:32 +0000

    12 Apr 2023, 13:13

--- src/Creational/Factory/InMemoryDatabase.ts
+++ src/Creational/Factory/InMemoryDatabase.ts
@@ -1,41 +1,6 @@
-
-type BaseClass={
-  id: string
+interface DatabaseServices<T>{
+   create(data:T):void
 }
 
-abstract class AbstractDatabase<T extends BaseClass>{
 
-     protected db:Record<string,T> ={}
-
-     abstract set(newValue:T):void
-     abstract get(id:string):T|undefined
-}
-
-class InMemoryDatabase<T extends BaseClass> extends AbstractDatabase<T>{
-    
-
-  get(id: string) {
-      console.log("The database is",this.db)
-      return this.db[id]
-  }
-  set(newValue: T): void {
-      this.db[newValue.id] = newValue
-  }
-
-}
-
-type Pokemon={
-  id: string,
-  attack:number,
-  power:number
-}
-const pokemonDb =  new InMemoryDatabase<Pokemon>() ;
-
-pokemonDb.set({
-  id:"bulbasaur",
-  attack:12,
-  power:14
-})
-
-console.log(pokemonDb.get("bulbasaur"))
 
