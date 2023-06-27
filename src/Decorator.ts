5fbb65b05b5eb4c3ef69ca85369dbeb698883779
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-27 05:30:08 +0000

    27 Jun 2023, 11:00

--- src/Decorator.ts
+++ src/Decorator.ts
@@ -1,6 +1,6 @@
 abstract class Car{
    
-     public description!:string
+     public abstract description:string
 
    
     public getDescription():string{
@@ -9,4 +9,59 @@ abstract class Car{
 
     public abstract cost() :number
       
+    }
+
+
+class ModelX extends Car{
+
+  public description='ModelX'
+
+  public cost(): number {
+    return 2000
+  }
+  
+}
+
+
+class ModelS extends Car{
+
+  public description='ModelS'
+
+  public cost(): number {
+    return 40000
+  }
+  
+}
+
+
+abstract class CarDecorator extends Car{
+     public abstract decoratedCar:Car
+     public abstract cost(): number
+     public abstract getDescription(): string
+
+
+
+    }
+
+
+
+class EnhancedAutopilot extends CarDecorator{
+
+  public description='enhanced autopilot'
+  public decoratedCar: Car
+  
+   constructor( decoratedCar:Car){
+    
+       super()
+
+        this.decoratedCar = decoratedCar
+     }
+
+  public cost(): number {
+    throw new Error("Method not implemented.")
+  }
+  public getDescription(): string {
+    throw new Error("Method not implemented.")
+  }
+  
 }
