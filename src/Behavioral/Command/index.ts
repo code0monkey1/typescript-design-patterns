3958c5da0d902f5d78be320845e35bdc3a7f8a90
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-20 09:09:30 +0000

    20 Aug 2023, 14:39

---
+++ src/Behavioral/Command/index.ts
@@ -0,0 +1,56 @@
+// Command Pattern (Behavioral):
+// Turns a request into a stand-alone object that contains all information about the request. This decouples sender and receiver.
+
+interface Command {
+  execute(): void;
+}
+
+class Light {
+  turnOn(): void {
+    console.log('Light is on');
+  }
+
+  turnOff(): void {
+    console.log('Light is off');
+  }
+}
+
+class LightOnCommand implements Command {
+  constructor(private light: Light) {}
+
+  execute(): void {
+    this.light.turnOn();
+  }
+}
+
+class LightOffCommand implements Command {
+  constructor(private light: Light) {}
+
+  execute(): void {
+    this.light.turnOff();
+  }
+}
+
+class RemoteControl {
+  private commands: Command[] = [];
+
+  addCommand(command: Command): void {
+    this.commands.push(command);
+  }
+
+  executeCommands(): void {
+    this.commands.forEach((command) => command.execute());
+  }
+}
+
+// Usage
+
+const light = new Light();
+const lightOnCommand = new LightOnCommand(light);
+const lightOffCommand = new LightOffCommand(light);
+
+const remoteControl = new RemoteControl();
+remoteControl.addCommand(lightOnCommand);
+remoteControl.addCommand(lightOffCommand);
+
+remoteControl.executeCommands();
